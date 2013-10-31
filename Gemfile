00271496945f6d974f35a4c5172a1749b4c7b5d0
Bill Kav <billkav19@yahoo.gr>
2013-10-31 20:38:06 +0200

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -8,12 +8,18 @@ gem 'rails', '4.0.1.rc3'
 group :development do
 	gem 'sqlite3', '1.3.8'
 	gem 'rspec-rails', '2.13.1'
+	gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
 	gem 'selenium-webdriver', '2.35.1'
 	gem 'capybara', '2.1.0'
+	gem 'spork-rails', '4.0.0'
+	gem 'guard-spork', '1.5.0'
+	gem 'childprocess', '0.3.6'
 end
+
+gem 'libnotify', '0.8.0'
 
 gem 'sass-rails', '4.0.0'
 
@@ -35,4 +41,6 @@ group :production do
 	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
 end
+
+
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+ 
+
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,8 +12,13 @@ describe "StaticPages" do
 
     it "should have the right title" do
 	  	visit '/static_pages/home'
-	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
   	end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
 
 
   end
@@ -24,7 +32,7 @@ describe "StaticPages" do
 
   	it "should have the right title" do
 	  	visit '/static_pages/help'
-	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	  	expect(page).to have_title("#{base_title} | Help")
   	end
 
   end
@@ -37,11 +45,22 @@ describe "StaticPages" do
 
   	it "should have the right title" do
 	  	visit '/static_pages/about'
-	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	  	expect(page).to have_title("#{base_title} | About Us")
   	end
 
   end
 
+  describe "Contact" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
 
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+  end
 
 end
