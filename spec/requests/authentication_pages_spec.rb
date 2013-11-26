b4ac416ddf3bb49011935fddcc1e095696d2572e
Basilis Kavasis <billkav19@yahoo.gr>
2013-11-26 15:27:52 +0200

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -58,8 +58,19 @@ describe "Authentication" do
 	    describe "for non-signed-in users" do
 	      let(:user) { FactoryGirl.create(:user) }
 
-	      describe "in the Micropost controller" do
+	      describe "in the Relationships controller" do
+	      	describe "submitting to the create action" do
+	      		 before { post relationships_path }
+	      		 specify { expect(response).to redirect_to(signin_path) }
+	      	end
 
+	      	describe "submitting to the destroy action" do
+	      		before { delete relationship_path(1) }
+	      		specify { expect(response).to redirect_to(signin_path) }
+	      	end
+	      end
+
+	      describe "in the Micropost controller" do
 	 		describe "submitting to the create action" do
 	 			before { post microposts_path }
 	 			specify { expect(response).to redirect_to(signin_path) }
@@ -68,8 +79,7 @@ describe "Authentication" do
 	 		describe "submitting to the destroy action" do
 	 			before { delete micropost_path(FactoryGirl.create(:micropost)) }
 	 			specify { expect(response).to redirect_to(signin_path) }
-	 		end
-	
+	 		end	
 	 	  end
 
 	      describe "when attempting to visit a protected page" do
@@ -105,8 +115,18 @@ describe "Authentication" do
 	        	before {visit users_path }
 	        	it { should have_title('Sign in') }
 	        end
-	      end
-	    end
+
+	        describe "visiting the following page" do
+	          before { visit following_user_path(user) }
+	          it { should have_title('Sign in') }
+	        end
+
+	        describe "visiting the followers page" do
+	          before { visit followers_user_path(user) }
+	          it { should have_title('Sign in') }
+	        end   
+	      end   
+		end
 
 	    describe "as wrong user" do
 	    	let(:user) {FactoryGirl.create(:user) }
